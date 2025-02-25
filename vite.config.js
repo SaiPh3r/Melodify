8780cb6396b0de5f5dead9953a213af4ecd14306
SaiPh3r <saianand750@gmail.com>
2025-02-25 14:03:40 +0530

    deploy

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(),react()],
-  base: "/your-repo-name/",
+  base: "/Melodify/",
 })
